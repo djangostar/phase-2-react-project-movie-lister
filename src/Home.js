a966bc51ffe67f7f54d2d5d999ca1cb9717604b3
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-05-03 10:28:30 -0700

    Implemented Home component and fixed index.js

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Home = () => {
+    return (
+        <div>
+            <h1>My Movie List</h1>
+            <hr/>
+            <p>Add and List Your Favorite Movies!</p>
+        </div>
+    )
+}
+
+export default Home
