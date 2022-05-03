0275369d11bd3a2e9db4931dbff84a8907a03fd3
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-05-03 10:12:50 -0700

    Starting static version of app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from "react"
+import { Route, Routes} from "react-router-dom"
 import './App.css';
+import Home from "./Home"
+import Movies from "./Movies"
+import Movie from "./Movie"
+import MovieForm from "./MovieForm"
+import About from "./About"
+import NavBar from "./NavBar"
 
-function App() {
+
+const App = () => {
+  
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/new" element={<MovieForm />} />
+          <Route path="/movie/:id" element={<Movie />} />
+          <Route path="/about" element={<About />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
