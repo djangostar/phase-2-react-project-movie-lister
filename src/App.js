f513b687aca6d92897a7ee2f8971fef2d36ada9c
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-05-03 10:19:05 -0700

    Wrote out the components needed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Route, Routes} from "react-router-dom"
 import './App.css';
 import Home from "./Home"
-import Movies from "./Movies"
+import MoviesList from "./Movies"
 import Movie from "./Movie"
 import MovieForm from "./MovieForm"
 import About from "./About"
@@ -18,7 +18,7 @@ const App = () => {
       <NavBar />
       <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies" element={<MoviesList />} />
           <Route path="/movies/new" element={<MovieForm />} />
           <Route path="/movie/:id" element={<Movie />} />
           <Route path="/about" element={<About />} />
